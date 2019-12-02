8df1e83ba8efa7dc49489e8ac86cd1a06969ef7c
Thái Thiên Vũ <thienvu1802@gmail.com>
2019-12-02 21:02:18 +0700

    Updates

---
+++ precache-manifest.c15516a5711df74d5cb0854956edbc98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d683b3dedf47ad28e114152891f294a6",
+    "url": "/hetea-fe/index.html"
+  },
+  {
+    "revision": "3685ad98dc2160372112",
+    "url": "/hetea-fe/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e8a24330bfed115fa190",
+    "url": "/hetea-fe/static/js/2.d1a41941.chunk.js"
+  },
+  {
+    "revision": "3685ad98dc2160372112",
+    "url": "/hetea-fe/static/js/main.fe34fa3e.chunk.js"
+  },
+  {
+    "revision": "f2187fc2f89ede4976d6",
+    "url": "/hetea-fe/static/js/runtime-main.f2f68442.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/hetea-fe/static/media/logo.25bf045c.svg"
+  }
+]);
